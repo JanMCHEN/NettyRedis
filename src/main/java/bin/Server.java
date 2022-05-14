e9ebdf813d1e29863aae7873bbc326dfef5dfe61
cjm <1933919646@qq.com>
2022-05-14 11:19:29 +0800

    update no

--- src/main/java/bin/Server.java
+++ src/main/java/bin/Server.java
@@ -10,6 +10,10 @@ import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.EventExecutorGroup;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class Server {
@@ -54,6 +58,13 @@ public class Server {
             bootstrap.childOption(ChannelOption.SO_KEEPALIVE, true);
 
             bootstrap.childHandler(new HandlerInit());
+            IntBuffer buffer = IntBuffer.allocate(1);
+            buffer.put(9);
+            int[] a = new int[1];
+            Arrays.stream(a).toArray();
+            "a".split(" ");
+
+
 
             bootstrap.bind(7000).sync().channel().closeFuture().sync();
         } catch (Throwable e) {
